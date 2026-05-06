41918e8453b2dfda4f6722cf7b04975a12cb6593
D00219688 <D00291688@student.dkit.ie>
2026-05-06 15:32:37 +0100

    llll

--- test_part1.py
+++ test_part1.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.mark.parametrize("input,expected",[(100,'3.00'),(200.50,'6.01'),(1450,'45.75'),(5670.56,'193.47'),(12000,'420.00'),(30000.78,'1140.03'),(130000,'5290.00'),(450000.23,'19690.01')])
 def test_calc_interest(input,expected):
     assert calculate_interest(input) == expected
+    
 
 @pytest.mark.parametrize("input",[("Hello"),(True),(-1),("")])
 def test_fails(input):
